5402d520ce99ee7b37f2dd702608eae1f7c5712f
Yatish1910 <andiyadav1910@gmail.com>
2023-04-11 13:02:10 +0530

    Testing commit.

--- app/src/main/java/com/example/bakewithlove/MainActivity.kt
+++ app/src/main/java/com/example/bakewithlove/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.bakewithlove
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val testingString = "Yatish Yadav"
+        Log.d("TestingPhase",testingString)
     }
 }
